4fa3fbfc9fd000a8a7c279b87eb67cd9f5d43b4a
Matt <matt.mui15@gmail.com>
2017-10-03 17:17:06 -0400

    replace about me to index

--- scripts/test.js
+++ scripts/test.js
@@ -1,2 +1,6 @@
 document.getElementsByClassName('header')[0].innerHTML = "Introduction"
 console.log('linked')
+
+document.getElementsByClassName('pskills')[0].addEventListener("click", function(m){
+  alert("contact me")
+})
